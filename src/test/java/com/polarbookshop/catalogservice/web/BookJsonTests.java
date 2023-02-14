32d1675730bf93b97de9a78f09153725e754ab8d
h4j4x <miche.atucha@gmail.com>
2023-02-13 20:53:19 -0500

    Book API

---
+++ src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -0,0 +1,44 @@
+package com.polarbookshop.catalogservice.web;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class BookJsonTests {
+    @Autowired
+    private JacksonTester<Book> json;
+
+    @Test
+    void testSerialize() throws Exception {
+        var book = new Book("1234567890", "Title", "Author", 9.90);
+        var jsonContent = json.write(book);
+        assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
+            .isEqualTo(book.isbn());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.title")
+            .isEqualTo(book.title());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.author")
+            .isEqualTo(book.author());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
+            .isEqualTo(book.price());
+    }
+
+    @Test
+    void testDeserialize() throws Exception {
+        var book = new Book("1234567890", "Title", "Author", 9.9);
+        var content = String.format("""
+            {
+            "isbn": "%s",
+            "title": "%s",
+            "author": "%s",
+            "price": %.2f
+            }
+            """, book.isbn(), book.title(), book.author(), book.price());
+        assertThat(json.parse(content))
+            .usingRecursiveComparison()
+            .isEqualTo(book);
+    }
+}
